d7c694e010469cd3ff545749a0c6f05792c83507
Rajdeep Unawane <152307246+RajdeepUnawane@users.noreply.github.com>
2026-02-12 11:26:46 +0530

    Delete server/Bom_DotNet directory

--- server/Bom_DotNet/DTOs/BomItemDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Bom.ReportingService.DTOs;
-
-    public class BomItemDto
-    {
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public List<BomItemDto> Children { get; set; }
-    }
